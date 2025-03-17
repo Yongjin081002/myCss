13644fc21e59ac5110129e58c08f46b38c7b16a7
Yongjin081002 <yg4059540@gmail.com>
2025-03-17 13:52:54 +0900

    Update index.jsx

--- src/components/SlideBanner/index.jsx
+++ src/components/SlideBanner/index.jsx
@@ -31,7 +31,7 @@ const SliderBanner = () => {
   return (
     <SliderWrapper>
       <Banner >
-        <img style={{width:"601px", height:"401px"}} src={banners[currentIndex].image} />
+        <img style={{width:"60px", height:"401px"}} src={banners[currentIndex].image} />
       </Banner>
       <DotsContainer>
         {banners.map((_, index) => (
